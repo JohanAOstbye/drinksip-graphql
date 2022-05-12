32a0332e55042b8a645f45a7364a31427920a243
Johan August Østbye <johan@ostbye.dev>
2022-05-12 11:53:12 +0200

    3. vercel format test

--- dev.ts
+++
@@ -1,7 +0,0 @@
-import httpServer from './api/index';
-import consola from 'consola';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-httpServer.listen(3000, () => consola.info('Server started'));
---
+++ index.ts
@@ -0,0 +1,37 @@
+import 'reflect-metadata';
+import express from 'express';
+import { createServer } from 'http';
+import { buildSchema } from 'type-graphql';
+import { ApolloServer } from 'apollo-server-express';
+import cors from 'cors';
+import { resolvers } from './graphql';
+import { prisma } from './prisma';
+
+const PORT = process.env.PORT || 4000;
+
+const start = async () => {
+  const app = express();
+  app.use(cors());
+  const schema = await buildSchema({
+    resolvers,
+  });
+
+  const apolloServer = new ApolloServer({
+    schema,
+    context: () => ({ prisma }),
+    introspection: true,
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({
+    app,
+    path: '/api',
+  });
+
+  app.listen(8080, () => {
+    console.log('Server started');
+  });
+};
+
+start();
